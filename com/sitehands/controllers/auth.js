d64c0fc1cad370cb2799ff01a2ffa9803fc832af
Raymond Bayly <raymond.bayly@outlook.com>
2020-05-03 12:24:35 -0400

    create the auth models

---
+++ com/sitehands/controllers/auth.js
@@ -0,0 +1,26 @@
+// this is the cookie engine
+
+
+const setUserToken = () => {
+    return true;
+}
+
+const expireUserToken = () => {
+    return true;
+}
+
+const getUserToken = () => {
+    return true;
+}
+
+const setAppToken = () => {
+    return true;
+}
+
+const expireAppToken = () => {
+    return true;
+}
+
+const getAppToken = () => {
+    return true;
+}

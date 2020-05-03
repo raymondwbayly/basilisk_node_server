d41a9c9debeaa7cb8531ec14745e783ab2970520
Raymond Bayly <raymond.bayly@outlook.com>
2020-05-03 18:08:28 -0400

    update config

--- com/core/controllers/config.controller.js
+++ com/core/controllers/config.controller.js
@@ -1,1 +1,18 @@
-// This is the message controller
+var path = require('path');
+var fs = require('fs');
+var lodash = require('lodash');
+
+var Configuration = require('../../../lib/core/models/Configuration');
+
+// Alerts JSON
+var configPath = path.join(__dirname, '../../../lib/data', 'config.json');
+var cfgJSON =  require(configPath);
+
+var ServerCfg = new Configuration(cfgJSON);
+
+
+const getConfig = () => {
+    return ServerCfg;
+}
+
+exports.getConfig = getConfig;
--- com/core/views/index/index.route.js
+++ com/core/views/index/index.route.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var cfgController = require('../../controllers/config.controller');
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Beowulf MSe Server', version: '0.1.0-alpha' });
+  var cfg = cfgController.getConfig();
+  res.render('index', { title: cfg.name, codename: cfg.codename,developer: cfg.developer,company: cfg.company, version: cfg.version });
 });
 
 module.exports = router;

2f4b0b3bd1870588deadb035d9b658b5a359d7cf
Raymond Bayly <raymond.bayly@outlook.com>
2020-05-04 10:43:51 -0400

    working on user service

---
+++ com/core/services/user/user.route.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+var userService = require('./user.service');
+var responseObj = require('../../controllers/response.controller');
+
+/* GET User listing. */
+router.get('/', function(req, res) {
+  res.send(responseObj.buildResponse('User', 'This is the default URI for the User Service'));
+});
+
+module.exports = router;
---
+++ com/core/services/user/user.service.js
@@ -0,0 +1,14 @@
+// This is the user service
+
+
+const verifyUserToken = (token) => {
+    return true;
+}
+
+const verifyDeviceToken = (token) => {
+    return true;
+}
+
+
+exports.verifyUserToken = verifyUserToken;
+exports.verifyDeviceToken = verifyDeviceToken;

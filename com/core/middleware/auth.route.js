9479d5aa191d8a56ebd6c215f8a820dbd714b43b
Raymond Bayly <raymond.bayly@outlook.com>
2020-05-04 10:38:09 -0400

    Create auth.route.js

---
+++ com/core/middleware/auth.route.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+function isAuthenticated(req, res, next) {
+  // do any checks you want to in here
+
+  // CHECK THE USER STORED IN SESSION FOR A CUSTOM VARIABLE
+  // you can do this however you want with whatever variables you set up
+  if (req.user.authenticated)
+      return next();
+
+  // IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM SOMEWHERE
+  res.redirect('/');
+}

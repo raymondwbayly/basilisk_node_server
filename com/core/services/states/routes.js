29108edc9a4d449062806a534f1a9ba9e6a99da7
Raymond Bayly <raymond.bayly@outlook.com>
2020-05-07 11:40:43 -0400

    update the states

--- com/core/services/states/routes.js
+++ com/core/services/states/routes.js
@@ -18,29 +18,4 @@ router.get('/:uid', function(req, res) {
   res.send(responseObj.buildResponse(core.getCategory(), 'Returns ' + core.getTitle() + ' based on ID', service.get(req.params.uid)));
 });
 
-/* GET Search Record */
-router.get('/search/:uid', function(req, res) {
-  res.send(responseObj.buildResponse(core.getCategory(), 'The ' + core.getTitle() + ' Searched for ' + req.params.uid, service.search(req.params.uid)));
-});
-
-/* POST Create Record */
-router.post('/', function(req, res) {
-  res.send(responseObj.buildResponse(core.getCategory(), 'Adds ' + core.getTitle() + ' to the Server ', service.add(req.body)));
-});
-
-/* PUT Update Record. */
-router.put('/', function(req, res) {
-  res.send(responseObj.buildResponse(core.getCategory(), core.getTitle() + '  ' + req.body.name  + ' Updated on the Server', service.update(req.body)));
-});
-
-/* DELETE Remove Record. */
-router.delete('/:uid', function(req, res) {
-  res.send(responseObj.buildResponse(core.getCategory(), 'Delete is disabled on this version', service.remove(req.params.uid)));
-});
-
-/* GET Update Active Flag. */
-router.get('/active/:uid', function(req, res) {
-  res.send(responseObj.buildResponse(core.getCategory(), ' Active Flag has been set ', service.active(req.params.uid)));
-});
-
 module.exports = router;
--- com/core/services/states/service.js
+++ com/core/services/states/service.js
@@ -18,51 +18,8 @@ const get = (uid) => {
     return db.get(records, uid);
 }
 
-const search = (value) => {
-    var records = core.getData();
-    var searchResults = [];
-    for(let r of records) {
-        if(r.name !== undefined){
-            if(r.code.toUpperCase() === value.toUpperCase() ) {
-                searchResults.push(r);
-            }
-        }
-    }
-    return searchResults;
-}
-
-const add = (record) => {
-    var lr = db.confirmFields(record);
-    var nState = new State(lr.id,lr.name,lr.abb,lr.code,lr.active);
-    return db.save(core.getData(), core.getPath(), nState.getJSON());
-}
-
-const update = (record) => {
-    var nState = new State( record.id, record.name, record.abb, record.code, record.active );
-    var records = core.getData();
-    var ind = db.findRecordIndex(records, nState.getID());
-    records[ind].name = nState.getName();
-    records[ind].abb = nState.getAbbreviation();
-    records[ind].code = nState.getCode();
-    records[ind].active = nState.getActive();
-    db.writeData(records, core.getPath());
-    return nState.getJSON();
-}
-
-const remove = (uid) => {
-    return db.remove(core.getData(), uid);
-}
-
-const active = (uid) => {
-    return db.checkActive(core.getData(), uid, core.getPath());
-}
 
 
 
 exports.list = list;
 exports.get = get;
-exports.search = search;
-exports.add = add;
-exports.update = update;
-exports.remove = remove;
-exports.active = active;

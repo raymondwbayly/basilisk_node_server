dcf8f30107cf45f0887adb08d639e3e92b3861bb
Raymond Bayly <raymond.bayly@outlook.com>
2020-05-07 14:29:27 -0400

    Update validators
    This is the validators

--- com/core/controllers/validation.js
+++ com/core/controllers/validation.js
@@ -1,5 +1,17 @@
 // Validator
 var emailValidator = require("email-validator");
+var passwordValidator = require('password-validator');
+
+var schema = new passwordValidator();
+
+schema
+.is().min(8)                                    // Minimum length 8
+.is().max(100)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+.has().digits()                                 // Must have digits
+.has().not().spaces()                           // Should not have spaces
+.is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
 
 
 
@@ -7,5 +19,15 @@ const validateEmail = (email) => {
     return emailValidator.validate(email);
 }
 
+const validatePassword = (password) => {
+    return schema.validate(password);
+}
+
+const passwordValidationDetails = (password) => {
+    return schema.validate(password, { list: true });
+}
+
 
 exports.validateEmail = validateEmail;
+exports.validatePassword = validatePassword;
+exports.passwordValidationDetails = passwordValidationDetails;
--- com/core/services/user/service.js
+++ com/core/services/user/service.js
@@ -5,6 +5,7 @@ var lodash = require('lodash');
 
 var db = require('../../controllers/database');
 var User = require('./model');
+var Validate = require('../../../../lib/core/models/Validate');
 var core = require('./index');
 var validators = require('../../controllers/validation');
 
@@ -33,32 +34,65 @@ const search = (value) => {
 }
 
 const add = (record) => {
+    var validateUser = new Validate();
+        validateUser.setValid(true);
     var lr = db.confirmFields(record);
-    if(validators.validateEmail(lr.email)){
+
+    // Validate the Email and Password Fields
+    if(!validators.validatePassword(record.password)){
+        validateUser.appendName('Password');
+        validateUser.appendMessage(validators.passwordValidationDetails(record.password));
+        validateUser.setValid(false);
+    }
+
+    if(!validators.validateEmail(record.email)){
+        validateUser.appendName('Email');
+        validateUser.appendMessage('Unable to Validate Email');
+        validateUser.setValid(false);
+    }
+
+    if(validateUser.getValid()){
         var nUser = new User(lr.id,lr.firstname, lr.lastname, lr.email, lr.password,lr.phone, lr.mobile,lr.profilepic, lr.active);
         return db.save(core.getData(), core.getPath(), nUser.getJSON());
     } else {
-        return ['The email is not a valid email address, please check it and try to submit the request again'];
+        return validateUser.getJSON();
     }
+
 }
 
 const update = (record) => {
-    if(validators.validateEmail(record.email)){
+    var validateUser = new Validate();
+        validateUser.setValid(true);
+
+    // Validate the Email and Password Fields
+    if(!validators.validatePassword(record.password)){
+        validateUser.appendName('Password');
+        validateUser.appendMessage(validators.passwordValidationDetails(record.password));
+        validateUser.setValid(false);
+    }
+
+    if(!validators.validateEmail(record.email)){
+        validateUser.appendName('Email');
+        validateUser.appendMessage('Unable to Validate Email');
+        validateUser.setValid(false);
+    }
+
+    if(validateUser.getValid()){
         var records = core.getData();
         var nUser = new User(record.id,record.firstname,record.lastname,record.email,record.password,record.phone,record.mobile,record.profilepic,record.active);
         var ind = db.findRecordIndex(records, record.id);
-        records[ind].firstname = nUser.getFirstName();
-        records[ind].lastname = nUser.getLastName();
-        records[ind].email = nUser.getEmail();
-        records[ind].password = nUser.getPassword();
-        records[ind].phone = nUser.getPhone();
-        records[ind].mobile = nUser.getMobile();
-        records[ind].profilepic = nUser.getProfilePic();
-        records[ind].active = nUser.getActive();
-        db.writeData(records, core.getPath());
+            records[ind].firstname = nUser.getFirstName();
+            records[ind].lastname = nUser.getLastName();
+            records[ind].email = nUser.getEmail();
+            records[ind].password = nUser.getPassword();
+            records[ind].phone = nUser.getPhone();
+            records[ind].mobile = nUser.getMobile();
+            records[ind].profilepic = nUser.getProfilePic();
+            records[ind].active = nUser.getActive();
+            db.writeData(records, core.getPath());
         return nUser.getJSON();
     } else {
-        return ['Email wasnt valid please check the email and resubmit'];
+            return validateUser.getJSON();
     }
 }
 
